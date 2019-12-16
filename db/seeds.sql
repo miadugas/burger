5c35c2ac1c62d54a0043680575d815e042fe1ded
Mia Dugas <miadugas@outlook.com>
2019-12-15 22:53:05 -0700

    burger

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+/* Real burgers from my favorite burger place in Atlanta, The Vortex */
+INSERT INTO burgers (name) VALUES ('Hot Southern Mess');
+INSERT INTO burgers  (name) VALUES ('Grilled Cheesy Mofo');
+INSERT INTO burgers  (name) VALUES ('Plain Ol Hamburger');
+INSERT INTO burgers  (name) VALUES ('Classic Bypass');
+INSERT INTO burgers  (name) VALUES ('Fat Elvis');
+INSERT INTO burgers  (name) VALUES ('Pickle Rick');
+INSERT INTO burgers  (name) VALUES ('Blue Shroom');
+INSERT INTO burgers  (name) VALUES ('Quadruple Coronary Bypass');
